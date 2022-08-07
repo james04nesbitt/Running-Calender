5f45d336b0165a68aa4f3d895aa956ca60d7fc1c
james04nesbitt <james04nesbitt@gmail.com>
2022-08-07 10:22:31 -0400

    s

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 function getMiles(){
-    const x = document.getElementById("mile").value;
-    document.getElementById("text").innerHTML = x;
+    var x = document.getElementById("mile").value;
+    calculate(x);
+}
+
+function calculate(miles){
+
+    const msf = miles;
+    if(miles >= 60){
+        const day6 = Math.round(miles/10);
+        document.getElementById("text").innerHTML = day6;
+    }
 }
