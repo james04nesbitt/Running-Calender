14c007a8da4ee69496b1cc052f92bfb2cab04de6
james04nesbitt <james04nesbitt@gmail.com>
2022-08-07 22:31:08 -0400

    FInished Product

--- index.js
+++ index.js
@@ -4,10 +4,34 @@ function getMiles(){
 }
 
 function calculate(miles){
+    const mwfs = Math.round(miles/9);
+    var day6 = Math.round(miles/9);
+    
+    var tt = Math.round(miles/17 ) + mwfs;
+   
+    const msf = mwfs * 4 + 2 * tt + day6;
+    
+    
+   
+    day6 = day6 +(miles-msf); 
+    if(miles < 60){
+        
+        tt = tt + mwfs/2;
 
-    const msf = miles;
-    if(miles >= 60){
-        const day6 = Math.round(miles/10);
-        document.getElementById("text").innerHTML = day6;
     }
+    
+        
+        document.getElementById("one").innerHTML = mwfs;
+        document.getElementById("two").innerHTML = tt;
+        
+        document.getElementById("three").innerHTML = mwfs;
+        document.getElementById("four").innerHTML = tt;
+        document.getElementById("five").innerHTML = mwfs;
+        document.getElementById("six").innerHTML = day6;
+
+        if(miles >= 60){
+            document.getElementById("seven").innerHTML = mwfs;
+        }
+        
+    
 }
